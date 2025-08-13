7df50180a630368f5e05910c063a24a2c06d8cc6
samuel <samawyck@gmail.com>
2025-08-13 15:56:22 -0500

    Create assetController

---
+++ controllers/assetController.js
@@ -0,0 +1,30 @@
+const asyncHandler = require("express-async-handler");
+const db = require("../db/querys.js");
+
+
+
+const getIconsGet = asyncHandler(async function(req, res) {
+    const images = await db.getManyImages({
+        select: {
+            iconUrl: true,
+            id: true
+        }
+    });
+
+    if (!images) {
+        return res.json({error: "Icons not found"});
+    }
+
+    for (let image of images) {
+        image.imageId = image.id;
+        delete image.id;
+    }
+
+    return res.json({images});
+});
+
+
+
+module.exports = {
+    getIconsGet
+};
