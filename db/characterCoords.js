96923a4e02803fdfee18f7ad84348d0c2d6c7d4e
samuel <samawyck@gmail.com>
2025-08-13 15:31:13 -0500

    Create base character local data

---
+++ db/characterCoords.js
@@ -0,0 +1,150 @@
+const characterCoords = [
+    {
+        character: "wilma",
+        image: "waldo-troy",
+        xCoord: 2705,
+        yCoord: 1610,
+        width: 40,
+        height: 50
+    },
+    {
+        character: "waldo",
+        image: "waldo-troy",
+        xCoord: 533,
+        yCoord: 1815,
+        width: 40,
+        height: 60
+    },
+    {
+        character: "wizard",
+        image: "waldo-troy",
+        xCoord: 990,
+        yCoord: 100,
+        width: 30,
+        height: 40
+    },
+    {
+        character: "waldo",
+        image: "waldo-movie-set",
+        xCoord: 3220,
+        yCoord: 1212,
+        width: 45,
+        height: 60
+    },
+    {
+        character: "wilma",
+        image: "waldo-movie-set",
+        xCoord: 2909,
+        yCoord: 1263,
+        width: 45,
+        height: 60
+    },
+    {
+        character: "wizard",
+        image: "waldo-movie-set",
+        xCoord: 2358,
+        yCoord: 1560,
+        width: 65,
+        height: 80
+    },
+    {
+        character: "waldo",
+        image: "waldo-giants",
+        xCoord: 610,
+        yCoord: 1453,
+        width: 60,
+        height: 90
+    }, 
+    {
+        character: "wilma",
+        image: "waldo-giants",
+        xCoord: 2436,
+        yCoord: 1768,
+        width: 55,
+        height: 80
+    },
+    {
+        character: "wizard",
+        image: "waldo-giants",
+        xCoord: 3396,
+        yCoord: 1687,
+        width: 95,
+        height: 80
+    },
+    {
+        character: "waldo",
+        image: "waldo-silent-movie",
+        xCoord: 157,
+        yCoord: 1575,
+        width: 40,
+        height: 50
+    },
+    {
+        character: "wilma",
+        image: "waldo-silent-movie",
+        xCoord: 951,
+        yCoord: 1347,
+        width: 45,
+        height: 50
+    },
+    {
+        character: "wizard",
+        image: "waldo-silent-movie",
+        xCoord: 2685,
+        yCoord: 289,
+        width: 60,
+        height: 70
+    },
+    {
+        character: "waldo",
+        image: "waldo-40-thieves",
+        xCoord: 2530,
+        yCoord: 1306,
+        width: 40,
+        height: 70
+    },
+    {
+        character: "wilma",
+        image: "waldo-40-thieves",
+        xCoord: 3015,
+        yCoord: 395,
+        width: 44,
+        height: 70
+    },
+    {
+        character: "wizard",
+        image: "waldo-40-thieves",
+        xCoord: 335,
+        yCoord: 1360,
+        width: 84,
+        height: 80
+    },
+    {
+        character: "waldo",
+        image: "waldo-three-musketeers",
+        xCoord: 1852,
+        yCoord: 1760,
+        width: 35,
+        height: 60
+    },
+    {
+        character: "wilma",
+        image: "waldo-three-musketeers",
+        xCoord: 1522,
+        yCoord: 1230,
+        width: 40,
+        height: 60
+    },
+    {
+        character: "wizard",
+        image: "waldo-three-musketeers",
+        xCoord: 3305,
+        yCoord: 625,
+        width: 68,
+        height: 90
+    }
+];
+
+
+
+module.exports = characterCoords;
